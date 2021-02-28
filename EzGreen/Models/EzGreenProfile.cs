3d37b4839ea052d16074370a9fc55731efe79c1f
יעקב גיגי <yaccovgigi@hotmail.com>
2021-02-28 20:25:21 +0200

    basic and testing - localstorage

---
+++ EzGreen/Models/EzGreenProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EzGreen.Models
+{
+    public class EzGreenProfile
+    {
+        public string ProfileName { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
