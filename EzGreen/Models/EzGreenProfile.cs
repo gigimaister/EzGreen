ec353d2bcb733fbac945aa7e58f6bb4aa1f8a5f6
יעקב גיגי <yaccovgigi@hotmail.com>
2021-03-02 16:05:29 +0200

    work-starting

--- EzGreen/Models/EzGreenProfile.cs
+++ EzGreen/Models/EzGreenProfile.cs
@@ -5,9 +5,25 @@ using System.Threading.Tasks;
 
 namespace EzGreen.Models
 {
+    
     public class EzGreenProfile
     {
+        private const int MAX_USERS = 10;
+
+        public int ProfileId { get; set; }
         public string ProfileName { get; set; }
         public string ProfilePicture { get; set; }
+
+        public bool MaxUsers(List<EzGreenProfile> ezGreenProfiles)
+        {
+            var numOfusers = ezGreenProfiles.Count();
+            if(numOfusers == MAX_USERS)
+            {
+                return true;
+            }
+            return false;
+        }
     }
+
+    
 }
