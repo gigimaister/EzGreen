2422752d6195be55ea3373114afe8c8b1008e18a
יעקב גיגי <yaccovgigi@hotmail.com>
2021-03-09 15:21:42 +0200

    adding list of profiles to Localstorage

--- EzGreen/Models/EzGreenProfile.cs
+++ EzGreen/Models/EzGreenProfile.cs
@@ -9,25 +9,17 @@ namespace EzGreen.Models
     
     public class EzGreenProfile
     {
-        private const int MAX_USERS = 10;
+        
 
         public int ProfileId { get; set; }
 
-        [Required(ErrorMessage = "חייב לבחור שם!")]
+        
         public string ProfileName { get; set; }
 
-        [Required(ErrorMessage = "חייב לבחור תמונה!")]
+        
         public string ProfilePicture { get; set; }
 
-        public bool MaxUsers(List<EzGreenProfile> ezGreenProfiles)
-        {
-            var numOfusers = ezGreenProfiles.Count();
-            if(numOfusers == MAX_USERS)
-            {
-                return true;
-            }
-            return false;
-        }
+       
     }
 
     
---
+++ EzGreen/Models/Logic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EzGreen.Models
+{
+    public static class Logic
+    {
+        private const int MAX_USERS = 10;
+        public static bool MaxUsers(int currenUser)
+        {    
+            if (currenUser > MAX_USERS)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
